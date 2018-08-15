950f9bce08842b1977e0b9d0a64f9d38c3805fc6
Brayan <bsgarciac@unal.edu.co>
2018-08-14 20:02:00 -0500

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component,  } from '@angular/core';
+import { NavController, ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  todoarray: any[];
+  card: any;
 
   constructor(public navCtrl: NavController) {
-
+    this.todoarray = [];
+    this.card = {
+      "header":"",
+      "body": "",
+    };
   }
 
+  createTask(){
+    this.card = {
+      "header":"University",
+      "body": "Study new words in German"
+    };
+    this.todoarray.push(this.card);
+  }
 }
